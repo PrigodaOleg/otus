35f260d28bd99aeb4d4ee2dbdb71fc11cc11cda8
prigoda <oleg.prigoda@kaspersky.com>
2021-07-19 23:02:57 +0300

    Parsing in generator function

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,6 +6,7 @@ import os
 import re
 import datetime
 import gzip
+import sys
 
 
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
@@ -44,28 +45,15 @@ def get_actual_log_file(path):
         candidate_file_name = os.path.join(path, candidate_file_name) if candidate_file_name else None
     else:
         raise AttributeError(f"Path {path} is not a directory, please specify log directory")
-    return candidate_file_name
+    return candidate_file_name, candidate_file_date
 
 
-def main():
-    # Get actual log file
-    logfile_name = get_actual_log_file(config['LOG_DIR'])
-    if logfile_name is None:
-        logging.warning('Log file has not found.')
-    else:
-        if os.path.isfile(os.path.join(config['REPORT_DIR'], f'{logfile_name}.log')):
-            logging.warning('Log file already exists, skip this time.')
-            return
+def parse_lines(file_name):
     # Try to open it according to its format
-    if logfile_name.endswith('.gz'):
-        logfile = gzip.open(logfile_name, 'rt')
-    else:
-        logfile = open(logfile_name, 'r')
+    logfile = gzip.open(file_name, 'rt') if file_name.endswith('.gz') else open(file_name, 'r')
+    skipped_lines = 0
+    total_lines = 0
     try:
-        c = 0
-        statistics = {}
-        total_request_time = 0
-        total_count = 0
         re_fields = re.compile(r'(\d+\.\d+\.\d+\.\d+) '                                 # ip
                                r'([\da-f]+|-)\s{1,2}'                                   # id?
                                r'(.|-) '                                                # ?
@@ -80,67 +68,75 @@ def main():
                                r'"(.+)" '                                               # ?
                                r'(\d+\.\d+)'                                            # request time
                                )
+        c = 200000
         for line in logfile:
+            total_lines += 1
             match = re_fields.match(line)
             if match:
-                ip, \
-                id_1, \
-                id_2, \
-                date, \
-                request, \
-                responce, \
-                id_3, \
-                url, \
-                source, \
-                id_5, \
-                id_6, \
-                id_7, \
-                request_time = match.groups()
-                request_time = float(request_time)
-                id = request.split()
-                id = id[1] if len(id) > 1 else id[0]
-                if id not in statistics:
-                    statistics[id] = {'count': 1,
-                                      'time_sum': request_time,
-                                      'time_avg': request_time,
-                                      'time_max': request_time,
-                                      'time_med': request_time}
-                else:
-                    statistics[id]['time_sum'] = round(statistics[id]['time_sum'] + request_time, 3)
-                    statistics[id]['time_avg'] = round((statistics[id]['time_avg'] *
-                                                        statistics[id]['count'] + request_time) /\
-                                                       (statistics[id]['count'] + 1), 3)
-                    statistics[id]['time_max'] = statistics[id]['time_max'] \
-                        if statistics[id]['time_max'] > request_time \
-                        else request_time
-                    statistics[id]['count'] += 1
-                total_request_time += request_time
-                total_count += 1
+                yield match.groups()
             else:
-                print(f'Line skipped: {line}')
-            c += 1
-            if c > 100000:
+                logging.info(f'Line skipped: {line}')
+                skipped_lines += 1
+            c -= 1
+            if not c:
                 break
-        report = []
-        for address in statistics:
-            statistics[address]['count_perc'] = round(statistics[address]['count'] / total_count * 100, 3)
-            statistics[address]['time_perc'] = round(statistics[address]['time_sum'] / total_request_time * 100, 3)
-            statistics[address]['url'] = address
-            report.append(statistics[address])
-
-        # Render report
-        report_template_filename = f"{config['LOG_DIR']}/report.html"
-        report_filename = f"{os.path.dirname(logfile_name)}/report_.html"
-        with open(report_template_filename, 'r') as template:
-            with open(report_filename, 'w') as report_file:
-                for line in template:
-                    if '$table_json' in line:
-                        line = line.replace('$table_json', str(report))
-                    report_file.write(line)
 
     finally:
         logfile.close()
+        errors_threshold = 50
+        error_lines_percentage = round(skipped_lines / total_lines, 3) * 100
+        if error_lines_percentage > errors_threshold:
+            logging.error(f'Too many errors occurred while parsing: {error_lines_percentage}%')
+            raise RuntimeError(f'Too many error lines in file {file_name}')
+
+
+def main():
+    # Get actual log file
+    logfile_name, logfile_date = get_actual_log_file(config['LOG_DIR'])
+    if logfile_name is None:
+        logging.info('Log file has not found.')
+    else:
+        if os.path.isfile(os.path.join(config['REPORT_DIR'], f'{logfile_name}.log')):
+            logging.info('Log file already exists, skip this time.')
+            return 1
+    statistics = {}
+    total_request_time = 0
+    total_count = 0
+    for ip, _, _, date, request, response, _, url, source, _, _, _, request_time in parse_lines(logfile_name):
+        request_time = float(request_time)
+        id = request.split()
+        id = id[1] if len(id) > 1 else id[0]
+        if id not in statistics:
+            statistics[id] = {'count': 1,
+                              'time_sum': request_time,
+                              'time_avg': request_time,
+                              'time_max': request_time,
+                              'time_med': request_time}
+        else:
+            statistics[id]['time_sum'] = round(statistics[id]['time_sum'] + request_time, 3)
+            statistics[id]['time_avg'] = round((statistics[id]['time_avg'] *
+                                                statistics[id]['count'] + request_time) /\
+                                               (statistics[id]['count'] + 1), 3)
+            statistics[id]['time_max'] = statistics[id]['time_max'] \
+                if statistics[id]['time_max'] > request_time \
+                else request_time
+            statistics[id]['count'] += 1
+        total_request_time += request_time
+        total_count += 1
+    report = []
+    for address in statistics:
+        statistics[address]['count_perc'] = round(statistics[address]['count'] / total_count * 100, 3)
+        statistics[address]['time_perc'] = round(statistics[address]['time_sum'] / total_request_time * 100, 3)
+        statistics[address]['url'] = address
+        report.append(statistics[address])
+
+    # Render report
+    report_template_filename = f"{config['LOG_DIR']}/report.html"
+    report_filename = f"{os.path.dirname(logfile_name)}/report-{logfile_date}.html"
+    with open(report_template_filename, 'r') as template:
+        with open(report_filename, 'w') as report_file:
+            report_file.write(template.read().replace('$table_json', str(report)))
 
 
 if __name__ == "__main__":
-    main()
+    sys,exit(main())
