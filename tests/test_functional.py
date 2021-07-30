8102bec59a00418b49e266ce6942118bdc2a31ca
prigoda <oleg.prigoda@kaspersky.com>
2021-07-30 17:35:52 +0300

    Add simple test

---
+++ tests/test_functional.py
@@ -0,0 +1,17 @@
+import unittest
+import log_analyzer
+import sys
+
+
+def test_args():
+    old_args = sys.argv
+    sys.argv = [old_args[0]]
+    try:
+        config = log_analyzer.get_config()
+        assert config['REPORT_SIZE'] == -1
+
+        sys.argv += ['--report_size', '100']
+        config = log_analyzer.get_config()
+        assert config['REPORT_SIZE'] == 100
+    finally:
+        sys.argv = old_args
